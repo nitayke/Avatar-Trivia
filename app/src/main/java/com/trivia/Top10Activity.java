9c761290a5d585a429976974217ac0e1cba3cb5b
Nitay Kessner <nitayke@gmail.com>
2020-10-04 14:26:54 +0300

    date saving

--- app/src/main/java/com/trivia/Top10Activity.java
+++ app/src/main/java/com/trivia/Top10Activity.java
@@ -1,27 +1,10 @@
 package com.trivia;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
-
 import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.Collections;
-
-import static com.trivia.MainActivity.ref;
 
 public class Top10Activity extends AppCompatActivity {
     TabLayout tabLayout;
